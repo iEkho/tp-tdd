06d8a5bba8f27713afc14b5b58a69be448287a8b
Francois <francois.desprez@hotmail.fr>
2015-03-30 17:14:26 +0200

    test

---
+++ src/main/java/iut/tdd/Convert.java
@@ -0,0 +1,39 @@
+package iut.tdd;
+
+import java.util.HashMap;
+
+public class Convert {
+	static HashMap<String, String> list = new HashMap<String, String>();
+	static {
+		list.put("0", "zéro");
+		list.put("1", "un");
+		list.put("2", "deux");
+		list.put("3", "trois");
+		list.put("4", "quatre");
+		list.put("5", "cinq");
+		list.put("6", "six");
+		list.put("7", "sept");
+		list.put("8", "huit");
+		list.put("9", "neuf");
+		list.put("10", "dix");
+		list.put("11", "onze");
+		list.put("12", "douze");
+		list.put("13", "treize");
+		list.put("14", "quatorze");
+		list.put("15", "quinze");
+		list.put("16", "seize");
+		list.put("20","vingt");
+		list.put("30", "trente");
+		list.put("40","quarente");
+		list.put("50","cinquante");
+		list.put("60","soixante");
+}
+
+	public static String num2text(String input) {
+		return list.get(input);
+	}
+
+	public static String text2num(String input) {
+	}
+
+}
